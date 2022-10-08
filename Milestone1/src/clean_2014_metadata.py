1bd19bde993bd9291d880598d4481c3f06bab69c
Francisco Rente <up201906761@up.pt>
2022-10-09 00:40:31 +0100

    Added html description removal and removed LS and PS

--- Milestone1/src/clean_2014_metadata.py
+++ Milestone1/src/clean_2014_metadata.py
@@ -3,14 +3,20 @@ import json
 import html
 import sys
 import unicodedata as ud
+import re
+
 import pandas as pd
 
 
 def clean_description(description):
     description = ud.normalize('NFKD', description).encode('ascii', 'ignore').decode('ascii')
     description = description.replace('`', "'")
+    description = description.replace('\'\'', "'")
     description = html.unescape(description)
     description = description.replace(u'\xa0', ' ')
+    # remove LS and PS
+    description = description.replace(u'\u2028', ' ')
+    description = description.replace(u'\u2029', ' ')
 
     return description
 
@@ -24,27 +30,27 @@ def clean_metadata(metadata):
 
     metadata = metadata.drop_duplicates(subset=['asin'])
 
-    if verbose_mode():
+    if verbose_mode() and initial_shape[0] != metadata.shape[0]:
         print("Removed " + str(initial_shape[0] - metadata.shape[0]) + " duplicate asin")
 
     metadata = metadata[metadata['price'] > 0]
 
-    if verbose_mode():
+    if verbose_mode() and initial_shape[0] != metadata.shape[0]:
         print("Removed " + str(initial_shape[0] - metadata.shape[0]) + " rows with price <= 0")
 
-    metadata = metadata[metadata['imUrl'] != 'no reference']
+    metadata = metadata[metadata['imgUrl'] != 'no reference']
 
-    if verbose_mode():
+    if verbose_mode() and initial_shape[0] != metadata.shape[0]:
         print("Removed " + str(initial_shape[0] - metadata.shape[0]) + " rows with no reference image")
 
-    metadata = metadata[metadata['imUrl'].str.contains('http')]
+    metadata = metadata[metadata['imgUrl'].str.contains('http')]
 
-    if verbose_mode():
+    if verbose_mode() and initial_shape[0] != metadata.shape[0]:
         print("Removed " + str(initial_shape[0] - metadata.shape[0]) + " rows with no http in image url")
 
     metadata = metadata[metadata['description'] != 'no description']
 
-    if verbose_mode():
+    if verbose_mode() and initial_shape[0] != metadata.shape[0]:
         print("Removed " + str(initial_shape[0] - metadata.shape[0]) + " rows with no description")
 
     # metadata = metadata[metadata['price'].notna()]
@@ -54,8 +60,14 @@ def clean_metadata(metadata):
     if verbose_mode():
         print("Cleaned description")
 
+    rex = re.compile(r'<href.*?>(.*?)</href>|<a.*?>(.*?)</a>|<ul.*?>(.*?)</ul>', re.S | re.M) # rex.match(data)
+    metadata = metadata[metadata['description'].apply(lambda x: rex.match(x) is None)]
+
+    if verbose_mode() and initial_shape[0] != metadata.shape[0]:
+        print("Removed " + str(initial_shape[0] - metadata.shape[0]) + " rows with html tags in description")
+
     final_shape = metadata.shape
-    if verbose_mode():
+    if verbose_mode() and initial_shape[0] != metadata.shape[0]:
         print("Removed " + str(initial_shape[0] - final_shape[0]) + " rows")
 
     if verbose_mode():
@@ -77,12 +89,12 @@ def parse_metadata():
             count += 1
             # if len(sys.argv) > 1 and sys.argv[1] == "-v" and count % 10000 == 0:
             #     print("Parsed " + str(count) + " lines")
-            # if count == 10000:
-            #     break
+            if count == 1000:
+                break
             dic = ast.literal_eval(line)
             metadata_list.append({'asin': dic['asin'],
                                   'price': dic['price'] if 'price' in dic else None,
-                                  'imUrl': dic['imUrl'] if 'imUrl' in dic else 'no reference',
+                                  'imgUrl': dic['imUrl'] if 'imUrl' in dic else 'no reference',
                                   'description': dic['description'] if 'description' in dic else 'no description'})
 
     if len(sys.argv) > 1 and sys.argv[1] == "-v":
@@ -100,10 +112,10 @@ def print_final_shape(metadata):
     print("Number of NA values in price: " + str(metadata['price'].isna().sum()))
     # print no of description == no description
     print("Number of description == no description: " + str((metadata['description'] == 'no description').sum()))
-    # print no of imUrl == no reference
-    print("Number of imUrl == no reference: " + str((metadata['imUrl'] == 'no reference').sum()))
-    # print no of imUrl not containing http
-    print("Number of imUrl not containing http: " + str((~metadata['imUrl'].str.contains('http')).sum()))
+    # print no of imgUrl == no reference
+    print("Number of imgUrl == no reference: " + str((metadata['imgUrl'] == 'no reference').sum()))
+    # print no of imgUrl not containing http
+    print("Number of imgUrl not containing http: " + str((~metadata['imgUrl'].str.contains('http')).sum()))
     print("--------------------")
 
 
