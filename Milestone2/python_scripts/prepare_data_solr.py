5a804a74888933bfb3c27db5ce1162985e39c839
dioma <dioma>
2022-11-06 23:53:51 +0000

    Added Milestone 2

---
+++ Milestone2/python_scripts/prepare_data_solr.py
@@ -0,0 +1,23 @@
+# Picks the datasets from the cleaned_data folder and converts them to one compact json
+
+# Reviews file path -> data/reviews.json
+# Metadata file path -> data/metadata.json
+
+#Importing important data
+import pandas as pd
+
+
+
+
+
+
+def main():
+    return
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
