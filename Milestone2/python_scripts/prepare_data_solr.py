f326136f61ef2d483d6470f02903dd452ce1834d
dioma <dioma>
2022-11-08 01:05:08 +0000

    Dual core working. Missing schema on 'asin' on reviews

--- Milestone2/python_scripts/prepare_data_solr.py
+++ Milestone2/python_scripts/prepare_data_solr.py
@@ -11,6 +11,8 @@ def read_dataframes():
     reviews = pd.read_json('data/reviews.json', lines=True)
     metadata = pd.read_json('data/metadata.json', lines=True)
     
+    reviews.insert(0, 'id',range(0, len(reviews)))
+    
 
     return reviews, metadata
 
@@ -22,16 +24,16 @@ def main():
     # Reads the dataframes
     reviews, metadata = read_dataframes()
     
-    # Change the dataframe to be ready to Solr
-    
+    # Change the dataframe to be ready to Solr    
     # Go through each metadata row and add the review to the metadata
-    metadata['review'] = metadata['asin'].apply(lambda x: reviews[reviews['asin'] == x])
-    
-    metadata.to_json('data/solr_data.json', orient='records', lines=True)
     
     # Create an 'id' column from 'asin' column
     metadata.rename(columns={'asin': 'id'}, inplace=True)
     
+    metadata.to_json('data/solr_metadata.json', orient='records', lines=True)
+    reviews.to_json('data/solr_reviews.json', orient='records', lines=True)
+    
+
     return
 
 
--- Milestone2/startup.sh
+++ Milestone2/startup.sh
@@ -2,21 +2,25 @@
 
 # Pre create the core
 precreate-core books
-
-
+precreate-core reviews
 
 # Start Solr in background mode so we can use the API to upload the schema
 solr start
 
-sleep 200
+sleep 10
 
 # Schema definition via API
-#curl -X POST -H 'Content-type:application/json' \
-#    --data-binary @/data/simple_schema.json \
-#    http://localhost:8983/solr/courses/schema
+curl -X POST -H 'Content-type:application/json' \
+    --data-binary @/schema_scripts/metadata_schema.json \
+    http://localhost:8983/solr/books/schema
+
+curl -X POST -H 'Content-type:application/json' \
+    --data-binary @/schema_scripts/reviews_schema.json \
+    http://localhost:8983/solr/reviews/schema
 
 # Populate collection
-bin/post -c books /data/metadata.json
+bin/post -c books /data/solr_metadata.json
+bin/post -c reviews /data/solr_reviews.json
 
 # Restart in foreground mode so we can access the interface
 solr restart -f
