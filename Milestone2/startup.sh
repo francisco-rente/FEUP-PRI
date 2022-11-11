270bf88a7d9675df34e45eb659c52b5115477705
Francisco Rente <up201906761@up.pt>
2022-11-11 19:37:05 +0000

    afternoon meeting aditions: schema refactor, auxiliar files and script modif

--- Milestone2/startup.sh
+++ Milestone2/startup.sh
@@ -8,13 +8,11 @@ solr start
 
 sleep 10
 
-# Schema definition via API
-curl -X POST -H 'Content-type:application/json' \
-    --data-binary @/schema_scripts/metadata_schema.json \
-    http://localhost:8983/solr/kindle/schema
+cp /schema_scripts/mysynonyms.txt /var/solr/data/kindle
+cp /schema_scripts/en_stopwords.txt /var/solr/data/kindle
 
 curl -X POST -H 'Content-type:application/json' \
-    --data-binary @/schema_scripts/reviews_schema.json \
+    --data-binary @/schema_scripts/kindle_schema.json \
     http://localhost:8983/solr/kindle/schema
 
 # Populate collection
